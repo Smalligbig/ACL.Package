245a830bb6555d0b9f77a6e847147d6626a6c96f
Smalligbig <smalligbig@gmail.com>
2024-09-17 09:05:03 +0800

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,2 +1,4 @@
+local Players = game:GetService("Players")
+local LocalPlr = Players.LocalPlayer
 local ntabn = ({ ... })[1] or { attach = Instance.new("NumberValue") }
-ntabn.attach.Value = 10
+ntabn.attach.Value = math.random(1, 10)
