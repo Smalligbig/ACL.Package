56673df49fdeb696aa949d7d22c976308caaf832
Smalligbig <smalligbig@gmail.com>
2024-09-18 17:24:20 +0800

    Create ACL.lua

---
+++ ACL.lua
@@ -0,0 +1,12 @@
+if setfflag then
+setfflag("AbuseReportScreenshot", "False")
+setfflag("AbuseReportScreenshotPercentage", "0")
+end
+local Players = game:GetService("Players")
+local LocalPlr = Players.LocalPlayer
+local Package1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Smalligbig/ACL.Package/main/MainModule.lua"))()
+wait()
+ 
+if Package1({ attach }) then
+ 
+end
